ebef8bcd3f4ced081f2956e5b1ff2b10640770fd
vaibhav-xi <vaibhav28890@gmail.com>
2025-09-01 03:16:40 +0000

    Balance DB Model

---
+++ models/Balance.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const balanceSchema = new mongoose.Schema({
+  userId: {
+    type: String,
+    ref: "User",
+    required: true,
+    unique: true,
+  },
+  BNB: { type: mongoose.Schema.Types.Decimal128, default: 0 },
+  USDT: { type: mongoose.Schema.Types.Decimal128, default: 0 },
+  USDC: { type: mongoose.Schema.Types.Decimal128, default: 0 },
+  BTC: { type: mongoose.Schema.Types.Decimal128, default: 0 },
+  LTC: { type: mongoose.Schema.Types.Decimal128, default: 0 },
+}, { timestamps: true });
+
+export default mongoose.model("Balance", balanceSchema);
