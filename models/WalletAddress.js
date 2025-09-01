22aad233d93d01ef9b14f19aceac7a38fb2601cc
vaibhav-xi <vaibhav28890@gmail.com>
2025-09-01 03:14:54 +0000

    Wallet Address DB Model

---
+++ models/WalletAddress.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const walletAddressSchema = new mongoose.Schema({
+  userId: {
+    type: String,
+    ref: "User",
+    required: true,
+  },
+  chain: {
+    type: String,
+    enum: ["bsc", "btc", "ltc"],
+    required: true,
+  },
+  asset: {
+    type: String,
+    enum: ["BNB", "USDT", "USDC", "BTC", "LTC"],
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  derivationPath: {
+    type: String,
+  },
+  idx: {
+    type: Number,
+    required: true,
+  },
+}, { timestamps: { createdAt: "created_at" } });
+
+export default mongoose.model("WalletAddress", walletAddressSchema);
